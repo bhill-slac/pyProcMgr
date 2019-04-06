0056ea7285f23953fae2fcd1997ad58bf211f92b
Bruce Hill <bhill@slac.stanford.edu>
2019-04-05 23:51:47 -0700

    WIP: launching process but trouble w/ running IOC detached.

--- pyProcMgr.py
+++ pyProcMgr.py
@@ -8,40 +8,140 @@
 #  Requested features to be added:
 #
 #==============================================================
+from __future__ import print_function
 import sys
 import os
+import io
+import locale
 #import socket
 import subprocess
 import argparse
 #import readline
 #import shutil
-#import tempfile
-#import textwrap
+import tempfile
+import textwrap
+import time
+
+procList = []
+
+def launchProcess( command, procNumber=1, verbose=False ):
+    # No I/O supported for these processes
+    # devnull = os.devnull
+    procEnv = {}
+    procEnv['PYPROC_ID'] = str(procNumber)
+    devnull = subprocess.DEVNULL
+    cmdArgs = ' '.join(command).split()
+    if verbose:
+        print( "launchProcess: %s\n" % cmdArgs )
+    procInput = tempfile.TemporaryFile( mode='w+' )
+    #procInput = subprocess.PIPE
+    procCreationFlags = 0
+    if hasattr( subprocess, "CREATE_NEW_CONSOLE" ):
+        procCreationFlags = subprocess.CREATE_NEW_CONSOLE
+    if hasattr( subprocess, "DETACHED_PROCESS" ):
+        procCreationFlags = subprocess.DETACHED_PROCESS
+    proc = None
+    testInput = "date >> /tmp/pyproc_%d.log\n" % procNumber
+    try:
+        if hasattr(procInput,"write"):
+            procInput.write( testInput )
+        proc = subprocess.Popen(	cmdArgs, stdin=procInput, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                                    env=procEnv, creationflags=procCreationFlags,
+                                    shell=True, executable="/bin/bash", universal_newlines=True )
+        if verbose:
+            print( "Launched PID: %d" % proc.pid )
+        (procOut,procErr) = proc.communicate( testInput )
+        proc.poll()
+        time.sleep(1)
+        if hasattr( proc.stdin, "write" ):
+            proc.stdin.write( testInput )
+        elif hasattr(procInput,"closed") and not procInput.closed:
+            procInput.write( testInput )
+        proc.poll()
+        (procOut,procErr) = proc.communicate( testInput )
+    except ValueError as e:
+        print( "launchProcess: ValueError" )
+        print( e )
+        pass
+    except OSError as e:
+        print( "launchProcess: OSError" )
+        print( e )
+        pass
+    except subprocess.CalledProcessError as e:
+        print( "launchProcess: CalledProcessError" )
+        print( e )
+        pass
+    except e:
+        print( "Unknown exception thrown" )
+        print( e )
+        pass
+    if verbose and proc is not None:
+        print( "Returning proc w/ PID: %d" % proc.pid )
+    return ( proc, proc.stdin )
+
+def killProcess( proc, verbose=False ):
+    if verbose:
+        print( "killProcess: %d\n" % proc.pid )
+    #proc.signal( os.sigKILL )
+    proc.kill()
 
 def process_options(argv):
     if argv is None:
         argv = sys.argv[1:]
-    description =	'pyProcMgr supports ...\n'
+    description =	'pyProcMgr supports launching one or more processes.\n' \
+                +	'Command strings w/ arguments should be quoted.\n' \
+                +	'pyProcMgr will run as long as any of it\'s child processes are still running,\n' \
+                +	'and if killed via Ctrl-C will kill any remaining child processes.'
     epilog_fmt  =	'\nExamples:\n' \
-                    'pyProcMgr --foo\n'
+                    'pyProcMgr pvget "-w1 TST:BaseVersion"\n'
     epilog = textwrap.dedent( epilog_fmt )
     parser = argparse.ArgumentParser( description=description, formatter_class=argparse.RawDescriptionHelpFormatter, epilog=epilog )
-    parser.add_argument( '-p', '--package',   dest='packages', action='append', \
-                        help='EPICS module-name/release-version. Ex: asyn/R4.30-1.0.1', default=[] )
-    parser.add_argument( '-f', '--input_file_path', action='store', help='Read list of module releases from this file' )
+    parser.add_argument( 'cmd',  help='Command to launch.  Should be an executable file.' )
+    parser.add_argument( 'arg', nargs='*', help='Arguments for command line. Enclose options in quotes.' )
+    parser.add_argument( '-c', '--count',  action="store", default=1, help='Number of processes to launch.' )
     parser.add_argument( '-v', '--verbose',  action="store_true", help='show more verbose output.' )
-    parser.add_argument( '--version',  		 action="version", version=eco_tools_version )
 
     options = parser.parse_args( )
 
     return options 
 
 def main(argv=None):
+    global procList
     options = process_options(argv)
+    args = ' '.join( options.arg )
+    if options.verbose:
+        print( "Cmd:  %s" % options.cmd )
+        print( "Args: %s" % options.arg )
+        print( "Full: %s %s" % ( options.cmd, args ) )
 
-    print "Done:"
+    try:
+        ( proc, procInput ) = launchProcess( [ options.cmd ] + options.arg, verbose=options.verbose )
+        if proc is not None:
+            procList.append( [ proc, procInput ] )
+    except:
+        pass
+
+    time.sleep(1)
+    print( "Waiting for %d processes:" % len(procList) )
+    for procPair in procList:
+        procPair[0].wait()
+
+    print( "Done:" )
     return 0
 
 if __name__ == '__main__':
-    status = main()
+    status = 0
+    try:
+        status = main()
+    except:
+        pass
+
+    for procPair in procList:
+        proc = procPair[0]
+        procInput = procPair[1]
+        if hasattr( procInput, 'close' ):
+            procInput.close()
+        if proc is not None:
+            killProcess( proc, verbose=True )
+
     sys.exit(status)
--- pyProcMgr.sh
+++ pyProcMgr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Make sure we have a canonical path to eco_tools
-this_script=`readlink -f $BASH_ARGV`
+#this_script=`readlink -f $BASH_ARGV`
+this_script=`readlink -f $0`
 this_dir=`readlink -f $(dirname $this_script)`
 
 $this_dir/pyProcMgr.py $*
