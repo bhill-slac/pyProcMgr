d9e30cb769d4fa24e54249d76473c5ad81166b83
Bruce Hill <bhill@slac.stanford.edu>
2019-04-08 22:16:37 -0700

    Add procServ env to launcher.

--- pyProcMgr.sh
+++ pyProcMgr.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Make sure we have procServ
+source /reg/g/pcds/pkg_mgr/etc/add_env_pkg.sh  procServ/2.7.0-1.3.0
+if [ ! -e "$(which procServ)" ]; then
+	echo Error: procServ not found!
+fi
+
 # Make sure we have a canonical path to eco_tools
 #this_script=`readlink -f $BASH_ARGV`
 this_script=`readlink -f $0`
