22796ba14f38c70bf1f290799474d2bd5226c260
Bruce Hill <bhill@slac.stanford.edu>
2019-04-10 17:22:16 -0700

    Adding some test db files and a pcds env setup script

---
+++ pcds_setup_py36env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /reg/g/pcds/package/conda/py36env.sh
+conda activate pva-gw
+source /reg/g/pcds/setup/epicsenv-bleeding_edge-7.0.sh 
+source /reg/g/pcds/pkg_mgr/etc/add_env_pkg.sh procServ/2.7.0-1.3.0
+
+P4P_TOP=/reg/neh/home/bhill/git-wa-neh/extensions/p4p-git 
+PY_LD_VER=`python $P4P_TOP/get_PY_LD_VER.py`
+
+#echo pythonpathmunge $PWD/python${PY_LD_VER}/${EPICS_HOST_ARCH}
+#source /reg/g/pcds/setup/pathmunge.sh
+pythonpathmunge $P4P_TOP/python${PY_LD_VER}/${EPICS_HOST_ARCH}
+
