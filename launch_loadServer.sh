04db33f8aba50373258c58668ddf33d727b64853
Bruce Hill <bhill@slac.stanford.edu>
2019-05-21 15:50:57 -0700

    Fix param handling in launch_loadServer.sh

--- launch_loadServer.sh
+++ launch_loadServer.sh
@@ -13,15 +13,23 @@ if [ ! -e "$LOADSERVER" ]; then
 fi
 
 PYPROCMGR_DIR=`readlink -f $(dirname ${BASH_SOURCE[0]})`
+PYPROCMGR=$PYPROCMGR_DIR/pyProcMgr.py 
+if [ ! -e "$PYPROCMGR" ]; then
+	echo "Error: pyProcMgr.py not found!"
+	exit 1
+fi
+echo PYPROCMGR = $PYPROCMGR_DIR 
+
 TOP=`readlink -f $(dirname $LOADSERVER)/../..`
-echo PYPROCMGR_DIR = $PYPROCMGR_DIR 
 echo TOP = $TOP
 
 cd $TOP
-#$PYPROCMGR_DIR/pyProcMgr.py -c 10 $LOADSERVER	\
-#	"-m 'DELAY=1.0,P=PVA:GW:TEST:$PYPROC_ID:,NELM=10'"	\
-#	"-d $TOP/db/drive_10Counters.db"
-$LOADSERVER	\
-	"-m 'DELAY=1.0,P=PVA:GW:TEST:$PYPROC_ID:,NELM=10'"	\
-	"-d $TOP/db/drive_10Counters.db"
+$PYPROCMGR -c 10 $LOADSERVER \
+	'-m DELAY=0.010,P=PVA:GW:TEST:$PYPROC_ID:,NELM=10'	\
+	'-d db/drive_100Counters.db'
+
+#$LOADSERVER	\
+#	-m DELAY=1.0,P=PVA:GW:TEST:1:,NELM=10	\
+#	-d $TOP/db/drive_10Counters.db
+
 
