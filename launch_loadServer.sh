5c6330f3b5e132d871ae08c2828589f1fd4c8bac
Bruce Hill <bhill@slac.stanford.edu>
2019-05-30 02:58:48 -0700

    Updated launch_loadServer.sh

--- launch_loadServer.sh
+++ launch_loadServer.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-TEST_DIR=/reg/d/iocData/gwTest1
-
 PROCSERV=`which procServ`
 if [ ! -e "$PROCSERV" ]; then
 	echo "Error: procServ not found!"
@@ -24,13 +22,18 @@ echo PYPROCMGR = $PYPROCMGR_DIR
 TOP=`readlink -f $(dirname $LOADSERVER)/../..`
 echo TOP = $TOP
 
+HOSTNAME=`hostname -s`
+TESTNAME=pva-ctrs0
+TEST_DIR=/reg/d/iocData/gwTest/$TESTNAME/$HOSTNAME
+mkdir -p $TEST_DIR
+cat /proc/cpuinfo > $TEST_DIR/cpuinfo
+cat /proc/meminfo > $TEST_DIR/meminfo
 cd $TOP
 $PYPROCMGR -c 10  \
 	-n loadServer -p 50000 \
 	-D $TEST_DIR \
 	$LOADSERVER \
-	'-m DELAY=0.010,P=PVA:GW:TEST:$PYPROC_ID:,NELM=10'	\
-	'-d db/drive_100Counters.db'
+	'-m DELAY=0.010,P=PVA:GW:TEST:$PYPROC_ID:,NELM=10 -d db/drive_100Counters.db'
 
 #$LOADSERVER	\
 #	-m DELAY=1.0,P=PVA:GW:TEST:1:,NELM=10	\
